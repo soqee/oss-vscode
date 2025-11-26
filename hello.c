49c1d2f4e818560a8116347a41819bd968a8bc94
soqee <soqee101@gmail.com>
2025-11-26 14:21:17 +0900

    Changed: hello.c(M), README.md(A)

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void main(){
-    prinf("Complete Installing VSCode..!\n");
+    printf("-------hello.c-------\n");
+    printf("Hi, It's my first repository created by VSCode.\n");
 }
